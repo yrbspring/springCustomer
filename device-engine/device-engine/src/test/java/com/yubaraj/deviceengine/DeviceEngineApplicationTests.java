5a6a49966189e994fa9ccc83c36f8afe97c2346e
yubaraj bhatta <yubaraj.bhatta@mavs.uta.edu>
2020-01-07 11:29:48 -0600

    device engine

---
+++ device-engine/device-engine/src/test/java/com/yubaraj/deviceengine/DeviceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubaraj.deviceengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
